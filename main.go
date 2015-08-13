93586d169ed124c68419e24afbbd581ce8746c81
Francois <francoishill11@gmail.com>
2015-08-13 15:38:30 +0200

    Added printing of own network/IP addresses.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -11,10 +13,37 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, world!\n")
 }
 
+func printIP() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Error getting IPs: " + err.Error())
+		return
+	}
+	// handle err
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			fmt.Println("Error getting IP address " + err.Error())
+			continue
+		}
+
+		// handle err
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				// ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+				fmt.Println("Found IP " + ip.String())
+			}
+
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/hello", HelloServer)
-	err := http.ListenAndServe(":45678", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	printIP()
+	log.Fatal(http.ListenAndServe(":45678", nil))
 }
